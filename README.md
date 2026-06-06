41c4912a822f3130333bb520822770731bf7f968
Finnegan Adair <finneganadair@gmail.com>
2026-06-06 11:14:51 -0400

    add disclaimer and finish up js

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ If you look at the commit logs, you can probably see my struggles with it, as I 
 
 ## Easter Egg
 - If you couldn't tell, the page is modeled after Arch Linux's site.
+
+## Disclaimer For AI
+ - Code Completion was used, and examples of code to implement certain features were looked at, but copy and pasting was avoided as much as possible.
+ - AI was mostly not used for debugging unless the process was recognized by me, and I asked the vscode AI to fix it; e.g. instantiation issues.
--- survey.js
+++ survey.js
@@ -11,32 +11,81 @@ const q4 = document.querySelector(".q4");
 const survey = document.querySelector(".survey");
 const result = document.querySelector(".result");
 
+let answer1 = null;
+let answer2 = null;
+let answer3 = null;
+let answer4 = null;
+
 // First Question
 ul_1.addEventListener("click", (e) => {
     if (e.target.tagName === "INPUT") {
-        let answer1 = e.target.value;
-        alert("answer 1: " + answer1);
+        answer1 = e.target.value;
     }
 });
 // Second
 ul_2.addEventListener("click", (e) => {
     if (e.target.tagName === "INPUT") {
-        let answer2 = e.target.value;
-        alert("answer 2: " + answer2);
+        answer2 = e.target.value;
     }
 });
 //Third
 ul_3.addEventListener("click", (e) => {
     if (e.target.tagName === "INPUT") {
-        let answer3 = e.target.value;
-        alert("answer 3: " + answer3);
+        if (e.target.value === "yes") {
+            answer3 = true;
+        } else {
+            answer3 = false;
+        }
     }
 });
-//Fourth
+// Fourth
 ul_4.addEventListener("click", (e) => {
     if (e.target.tagName === "INPUT") {
-        let answer4 = e.target.value;
-        alert("answer 4: " + answer4);
+        answer4 = e.target.value;
     }
 });
+
+
+//from here on out, we will try to use jquery so i can learn it.
+$(document).ready(function() {
+    $("#submit").on('click', function() {
+        getResult();
+    });
+});
+
+function getResult()
+{
+    if(!(answer1 == null || answer2 == null || answer3 == null || answer4 == null)) {
+        if(answer1 === "gaming"){
+            if(answer4 === "high"){
+                $('#distro').text("Windows 11 - since the bloat won't bottleneck you too much, and it has higher gaming support than Linux.")
+            }
+            else {
+                $('#distro').text("Bazzite - it's a gaming focused distro that is very user friendly, and should run well on medium performance PCs.")
+            }
+        }
+        else if (answer1 === "programming"){
+            if(answer2 === "oob"){
+                $('#distro').text("Ubuntu - it's a very user friendly distro with good support for programming, and is very easy to set up.")
+            }
+            else {
+                $('#distro').text("Arch Linux - it's a very customizable distro that is great for programming, but it requires a lot of setup.")
+            }
+        }
+        else if (answer1 === "general"){
+            if(answer3 === true){
+                $('#distro').text("Fedora - it's a very user friendly distro that is great for general use, and has good support for the command line.")
+            }
+            else {
+                $('#distro').text("Linux Mint - it's a very user friendly distro that is great for general use, and has a very simple interface.")
+            }
+        }
+
+    }
+    else { 
+        alert("Please answer all questions before submitting the survey.");
+    }
+}
+
+
 
